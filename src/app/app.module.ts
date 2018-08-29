ec8187e42f50501a2b38cb8ced3d5ab80025fdb0
MarMilli <42286256+MarMilli@users.noreply.github.com>
2018-08-29 16:27:39 +0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {BoardComponent} from './board/board.component';
+import {StageComponent} from './stage/stage.component';
+import {TaskComponent} from './task/task.component';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardComponent,
+    StageComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
